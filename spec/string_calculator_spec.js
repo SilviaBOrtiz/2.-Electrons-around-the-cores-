241ef5c9a9a37d0dd63859cd5cfa92b1a50c91eb
SilviaBOrtiz <silviariasortiz@gmail.com>
2025-06-17 16:02:49 +0100

    test: add failing test for input [1,2,3,4,5] → 6 (Driver: Silvia)

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -1,9 +1,7 @@
-describe('String Calculator', function() {
-	beforeEach(function() {
-		calculator = new StringCalculator();
-	});
+describe("Electrons Around the Cores", function () {
 
-	it('should return 0 for an empty string', function() {
-		expect(calculator.add('')).toEqual(0);
-	});
+  it("returns 6 for [1, 2, 3, 4, 5]", function () {
+    expect(electronsAroundCores([1, 2, 3, 4, 5])).toBe(6);
+  });
+
 });
